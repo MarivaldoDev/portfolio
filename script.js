55662737c15fd3522d5e45ca6911cad04fe39976
MarivaldoDev <pedromarivaldo10@gmail.com>
2024-04-18 21:00:30 -0300

    'foi'

---
+++ script.js
@@ -0,0 +1,15 @@
+const typed = new Typed('.multiple-text', {
+  strings: ['Estudante da área de TI', 'Desenvolvedor Backend.'],
+  typeSpeed: 70,
+  backSpeed: 70,
+  backDelay: 1000,
+  loop: true,
+});
+
+const vai = new Typed('.passar', {
+  strings: ['Passe para o lado ->'],
+  typeSpeed: 70,
+  backSpeed: 70,
+  backDelay: 3000,
+  loop: true,
+});
